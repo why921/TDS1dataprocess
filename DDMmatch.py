96b94404eb4419ab6b89943836122d217b71b3ab
why <359226737@qq.com>
2023-04-04 21:33:10 +0800

    DDM

---
+++ DDMmatch.py
@@ -0,0 +1,8 @@
+import netCDF4 as nc
+from netCDF4 import Dataset
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+a=np.load('test.npy')
--- L1BDDM.py
+++ L1BDDM.py
@@ -7,14 +7,24 @@ import os
 
 #E:\GNSSR_DATA\TDS-1\L1B\2018-12\01\H00
 
-DDMs_path = r'E:\GNSSR_DATA\TDS-1\L1B\2018-12\01\H00\DDMs.nc'
-
-DDMs_dataset = Dataset(DDMs_path)
-for group_name in DDMs_dataset.groups:
-    group1 = DDMs_dataset.groups[group_name]
 
 
-metadata_path=r'E:\GNSSR_DATA\TDS-1\L1B\2018-12\01\H00\metadata.nc'
-metadata_dataset = Dataset(metadata_path)
-for group_name in metadata_dataset.groups:
-    group2 = metadata_dataset.groups[group_name]
+
+def DDMs(path):
+    DDMs_dataset = Dataset(path)
+    for group_name in DDMs_dataset.groups:
+        group1 = DDMs_dataset.groups[group_name]
+    return group1
+
+
+def lonlat(path):
+    metadata_dataset = Dataset(path)
+    for group_name in metadata_dataset.groups:
+        group2 = metadata_dataset.groups[group_name]
+
+
+
+if __name__ == "__main__":
+    DDMs_path = r'E:\GNSSR_DATA\TDS-1\L1B\2018-12\01\H00\DDMs.nc'
+    metadata_path = r'E:\GNSSR_DATA\TDS-1\L1B\2018-12\01\H00\metadata.nc'
+    print(DDMs(DDMs_path))
